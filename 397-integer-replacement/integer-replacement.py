c0817a0c6684a99279312760a27e79427a306d87
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-10-14 15:34:33 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.59%) - LeetSync

--- 397-integer-replacement/integer-replacement.py
+++ 397-integer-replacement/integer-replacement.py
@@ -4,12 +4,11 @@ class Solution:
         while n > 1:
             if n % 2 == 0:
                 n //=2
-            elif n == 3 or (n & 2) == 0:
+            elif n == 3 or (n+1) % 4 != 0:
                 n -= 1
             else:
                 n += 1
             count += 1
-            
             
         return count
             

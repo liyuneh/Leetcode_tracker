05fe41656c44081848157d87efdeb0250ce1df91
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-10-26 22:22:50 +0300

    Time: 7 ms (5.61%) | Memory: 18 MB (21.27%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        odd, even = 0, 0
+        for i in range(len(num)):
+            if i % 2 == 1 :
+                odd += int(num[i])
+            else:
+                even += int(num[i])
+        return odd == even

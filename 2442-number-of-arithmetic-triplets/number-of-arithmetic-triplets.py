583c34adcdf76779dc06440471eee77f1ccb039c
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-11-08 20:53:31 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.12%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,10 +1,9 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        countin = 1
-        countout = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    if nums[j] - nums[i] == nums[k] - nums[j] == diff:
-                        countout += 1
-        return countout
+        count = 0
+        seen = set(nums)
+
+        for x in nums:
+            if x + diff in seen and x + 2 * diff in seen:
+                count += 1
+        return count

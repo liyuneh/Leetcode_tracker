bfba9439fb537c74cbeb0a4402c5ce240e05df68
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-11-25 16:12:47 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (68.07%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -11,7 +11,7 @@ class Solution:
             two = int(s[i-2:i])
 
             if 1 <=  one <= 9:
-                dp[i] += dp[i-1]
+                dp[i] = dp[i-1]
             
             if 10 <= two <= 26:
                 dp[i] += dp[i-2]

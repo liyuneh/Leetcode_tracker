2d2160edeaa9f3adfbf382e78ac3335cde732d16
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-11-28 13:12:46 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.45%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
@@ -3,7 +3,8 @@ class Solution:
         pref = ""
         for i in range(len(s)):
             if s[i].isdigit():
-                pref += chr((ord(s[i-1]) - ord('a') + (int(s[i]) % 26)) % 26 + ord('a'))
+               pref += chr((ord(s[i-1]) - ord('a') + int(s[i])) % 26 + ord('a'))
+
 
             else:
                 pref += s[i]

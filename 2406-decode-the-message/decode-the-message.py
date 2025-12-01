3a1c7df3e70fd42bf46561d7a3effdcad4d5042c
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-12-01 20:11:26 +0300

    Time: 3 ms (51.84%) | Memory: 17.7 MB (95.36%) - LeetSync

--- 2406-decode-the-message/decode-the-message.py
+++ 2406-decode-the-message/decode-the-message.py
@@ -1,18 +1,19 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        key = key.replace(" ", "")
-        sub = [ch for ch in key.lower()]
+        key = key.replace(" ", "").lower()
         unique = []
         seen = set()
-        for ch in sub:
+        for ch in key:
             if ch not in seen:
                 unique.append(ch)
                 seen.add(ch)
-        mapp = {ch: chr(97 + i) for i, ch in enumerate(unique)}
+        map = {}
+        for i , ch in enumerate(unique):
+            map[ch] = chr(ord('a') + i)
         s = ""
-        for i in range(len(message)):
-            if message[i].isalpha():
-                s += mapp[message[i]]
+        for ch in message:
+            if ch.isalpha():
+                s += map[ch]
             else:
                 s += " "
         return s

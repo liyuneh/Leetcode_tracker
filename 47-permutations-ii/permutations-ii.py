7cc7a1c9b524aa80e4b299e414a90e92e53286d1
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2025-12-27 22:04:04 +0300

    Time: 19 ms (21.60%) | Memory: 17.6 MB (96.05%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        p = permutations(nums)
+        ans = []
+        seen = set()
+        for prem in p:
+            t = tuple(prem)
+            if t not in seen:
+                seen.add(t)
+                ans.append(list(t))
+        return ans

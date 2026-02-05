0761f7d93e7a51eca3d9592023594ecd88510dba
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-02-05 15:13:40 +0300

    Time: 631 ms (5.50%) | Memory: 19.4 MB (20.83%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        freq = {}
+        taken = {}
+
+        for i , n in enumerate(secret):
+            freq[i] = n
+        for i , n in enumerate(guess):
+            taken[i] = n
+        bulls = 0
+        seen = set()
+        for i in range(len(guess)):
+            if guess[i] == freq[i]:
+                bulls += 1
+                seen.add(i)
+                del freq[i]
+                del taken[i]
+        cows = 0
+        for i in range(len(guess)):
+            for key , val in freq.items():
+                if val == guess[i] and i not in seen:
+                    cows += 1
+                    freq[key] = "visisted"
+                    seen.add(i)
+
+        return f"{bulls}A{cows}B"

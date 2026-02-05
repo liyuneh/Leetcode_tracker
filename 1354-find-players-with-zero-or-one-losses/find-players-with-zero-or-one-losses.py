3f3d7970d9acc00592c818a3f9369ad7cf56c0fc
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-02-05 11:54:13 +0300

    Time: 181 ms (14.03%) | Memory: 64.4 MB (16.69%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -1,20 +1,24 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         losers = {}
-        winners = {}
+        ans = []
+        seen = set()
         for winner, loser in matches:
-            winners[winner] = winners.get(winner, 0) + 1
+            if winner not in seen:
+                ans.append(winner)
+                seen.add(winner)
+            if loser not in seen:
+                seen.add(loser)
+                ans.append(loser)
             losers[loser] = losers.get(loser, 0) + 1
-        ans1 , ans2 = [], []
-        ans3 = []
-        for key , values in winners.items():
-            if losers.get(key, 0) == 0:
-                ans1.append(key)
-        ans3.append(sorted(ans1))
-        for key , values in losers.items():
-            if losers.get(key, 0) == 1:
-                ans2.append(key)
-        ans2.sort()
+        ans.sort()
+        ans3 , ans2, ans1 = [], [], []
+        for i in range(len(ans)):
+            if losers.get(ans[i], 0) == 0:
+                ans1.append(ans[i])
+            elif losers.get(ans[i], 0) == 1:
+                ans2.append(ans[i])
+        ans3.append(ans1)
         ans3.append(ans2)
         return ans3
             

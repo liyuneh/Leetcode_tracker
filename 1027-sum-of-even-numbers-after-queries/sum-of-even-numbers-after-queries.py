70617c5fb4cb90cfa4120a31c2c48a7620c77067
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-02-09 18:15:40 +0300

    Time: 27 ms (18.91%) | Memory: 24.3 MB (12.61%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.py
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.py
@@ -4,16 +4,11 @@ class Solution:
         ans = []
         for val, index in queries:
             if nums[index] % 2 == 0 and (nums[index] + val) % 2 == 0:
-                nums[index] = nums[index] + val
                 mx += val
             elif nums[index] % 2 == 1 and (nums[index] + val) % 2 == 0:
                 mx += (nums[index] + val)
-                nums[index] = nums[index] + val
-                print(nums[index])
             elif nums[index] % 2 == 0 and (nums[index] + val) % 2 == 1:
                 mx -= nums[index]
-                nums[index] = nums[index] + val
-            else:
-                nums[index] = nums[index] + val
+            nums[index] = nums[index] + val
             ans.append(mx)
         return ans

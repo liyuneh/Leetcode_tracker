e6fd68c50c12f8a7b9929c63aced022ae326ebea
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-02-18 18:18:50 +0300

    Time: 19 ms (53.13%) | Memory: 19.2 MB (64.01%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
         numss = sorted(nums)
         ans = []
         for num in nums:

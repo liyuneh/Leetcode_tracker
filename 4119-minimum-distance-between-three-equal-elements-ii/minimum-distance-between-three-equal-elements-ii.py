1f72a8347c9e7150afa1e5c60c97ab3192358a00
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-03 16:14:46 +0300

    Time: 435 ms (30.25%) | Memory: 51.2 MB (17.65%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        freq = defaultdict(list)
+        for i in range(len(nums)):
+            freq[nums[i]].append(i)
+        ans = defaultdict(list)
+        for key , val in freq.items():
+            if len(val ) >= 3:
+                ans[key] = val
+        # print(ans)
+
+        mx = max(len(val) for val in freq.values())
+        if mx < 3:
+            return - 1
+        mx = float("inf")
+        for key , val in ans.items():
+            mn = float("inf")
+            for i in range(len(val) - 2):
+                x = abs(val[i] - val[i+1]) + abs(val[i] - val[i+2]) + abs(val[i+1] - val[i+2])
+                mn = min(mn , x)
+            mx = min(mx, mn)
+        # print(mx)
+        return mx

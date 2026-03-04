80c05b041b8e42f15e7f10f4964f11e21e07981b
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-04 17:39:28 +0300

    Time: 152 ms (20.52%) | Memory: 34.3 MB (17.68%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -2,8 +2,6 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
-        # for i in range(len(matrix)):
-        #     print(matrix[i])
         self.prefs = [[0] * len(matrix[0]) for _ in range(len(matrix))] 
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[0])):
@@ -11,8 +9,6 @@ class NumMatrix:
                 b = self.prefs[i][j-1] if j - 1 >= 0 else 0
                 c = self.prefs[i-1][j-1] if i - 1 >= 0 and j - 1 >= 0 else 0
                 self.prefs[i][j] = a + b - c + matrix[i][j]
-        for i in range(len(self.prefs)):
-            print(self.prefs[i])
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         a = self.prefs[row1 - 1][col1 - 1] if row1 - 1 >= 0 and col1 - 1 >= 0 else 0

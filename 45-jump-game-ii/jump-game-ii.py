b22add3fdb74f38f8e3514fe0c34c9a57f691155
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-13 16:44:19 +0300

    Time: 7 ms (53.56%) | Memory: 20.1 MB (57.37%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -4,7 +4,6 @@ class Solution:
             return 0
         opt = 1
         ans = [[nums[0], 0]]
-        prev = 0
         for i in range(1,len(nums)):
             if not ans:
                 return opt
@@ -14,13 +13,11 @@ class Solution:
                 mx = ans[-1][0]
                 b = ans[-1][1]
                 x, y = ans.pop()
-                prev = -1
                 for j in range(y  + 1, (x + y) + 1):
                     if nums[j] + j > mx + b:
                         mx = nums[j]
                         b = j
                 if mx and b:
-                    print(mx,b)
                     ans.append([mx,b])
                     opt += 1
         return 

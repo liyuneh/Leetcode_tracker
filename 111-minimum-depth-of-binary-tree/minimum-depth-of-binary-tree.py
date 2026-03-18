7c14c4420444c383f78b1f72947a7562c4f3a657
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-18 18:12:20 +0300

    Time: 51 ms (25.38%) | Memory: 45 MB (41.86%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,13 +6,26 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        cur = float('inf')
-        if root.left:
-            cur = min(cur, self.minDepth(root.left))
-        if root.right:
-            cur = min(cur, self.minDepth(root.right))
-        return cur + 1
+        # if not root:
+        #     return 0
+        # if not root.left and not root.right:
+        #     return 1
+        # cur = float('inf')
+        # if root.left:
+        #     cur = min(cur, self.minDepth(root.left))
+        # if root.right:
+        #     cur = min(cur, self.minDepth(root.right))
+        # return cur + 1
+        if not root: return 0
+        stack = [(root, 0)]
+        ans = float("inf")
+        while stack:
+            node, h = stack.pop()
+            if not node.left and not node.right:
+                ans = min(ans, h + 1)
+                continue
+            if node.left:
+                stack.append((node.left, h + 1))
+            if node.right:
+                stack.append((node.right, h + 1))
+        return ans

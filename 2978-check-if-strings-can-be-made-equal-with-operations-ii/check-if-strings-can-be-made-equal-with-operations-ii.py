84d584d1176cf8414ce122a493dbffa41ff0b521
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-30 11:48:04 +0300

    Time: 241 ms (7.46%) | Memory: 30 MB (6.72%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        if Counter(s1) != Counter(s2):
+            return False
+        ans, ans1 = defaultdict(list), defaultdict(list)
+        for i , ch in enumerate(s1):
+            ans[ch].append(i + 1)
+        for i, ch in enumerate(s2):
+            ans1[ch].append(i + 1)
+        for key, val in ans1.items():
+            for i in range(len(val)):
+                ans[key].append(val[i])
+        for key, val in ans.items():
+            counteven, countodd = 0, 0
+            for i in range(len(val)):
+                if val[i] % 2 == 0:
+                    counteven += 1
+                else:
+                    countodd += 1
+            if counteven % 2 != 0 or countodd % 2 != 0:
+                return False
+        
+        return True
+        

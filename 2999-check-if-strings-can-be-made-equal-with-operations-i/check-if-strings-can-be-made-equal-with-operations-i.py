2984c9a199036388370f64bfc88880291257055d
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-30 11:49:37 +0300

    Time: 7 ms (2.79%) | Memory: 19.4 MB (41.34%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,4 +1,24 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        return sorted([s1[0], s1[2]]) == sorted([s2[0], s2[2]]) and \
-               sorted([s1[1], s1[3]]) == sorted([s2[1], s2[3]])
+        if Counter(s1) != Counter(s2):
+            return False
+        ans, ans1 = defaultdict(list), defaultdict(list)
+        for i , ch in enumerate(s1):
+            ans[ch].append(i + 1)
+        for i, ch in enumerate(s2):
+            ans1[ch].append(i + 1)
+        for key, val in ans1.items():
+            for i in range(len(val)):
+                ans[key].append(val[i])
+        for key, val in ans.items():
+            counteven, countodd = 0, 0
+            for i in range(len(val)):
+                if val[i] % 2 == 0:
+                    counteven += 1
+                else:
+                    countodd += 1
+            if counteven % 2 != 0 or countodd % 2 != 0:
+                return False
+        
+        return True
+        

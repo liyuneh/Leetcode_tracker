b4869dbd84a34ed900a8a0ef4f73b90c58e34000
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-31 22:39:14 +0300

    Time: 3 ms (20.74%) | Memory: 19.4 MB (21.36%) - LeetSync

---
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        res = None
+        path = []
+        used = [False] * 10
+
+        def backtrack():
+            nonlocal res
+            if res is not None:
+                return
+            if len(path) == len(pattern) + 1:
+                res = "".join(path)
+                return
+            for i in range(1, 10):
+                if used[i]:
+                    continue
+                if path:
+                    prev = int(path[-1])
+                    if pattern[len(path) - 1] == 'I' and prev >= i:
+                        continue
+                    if pattern[len(path) - 1] == 'D' and prev <= i:
+                        continue
+                used[i] = True
+                path.append(str(i))
+                backtrack()
+                path.pop()
+                used[i] = False
+
+        backtrack()
+        return res

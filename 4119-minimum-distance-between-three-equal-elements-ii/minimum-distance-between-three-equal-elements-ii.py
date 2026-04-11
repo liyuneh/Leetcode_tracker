d6f3ecfbd1e3fde6ae2f6e6d9bb2ffd428a261ca
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-04-11 21:47:26 +0300

    Time: 345 ms (63.64%) | Memory: 50.6 MB (54.54%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -1,21 +1,16 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         freq = defaultdict(list)
-        for i in range(len(nums)):
-            freq[nums[i]].append(i)
-        ans = defaultdict(list)
+        for i , ch in enumerate(nums):
+            freq[ch].append(i)
+        if not any(len(val) >= 3 for val in freq.values()):
+            return -1
+        mn = float("inf")
         for key , val in freq.items():
-            if len(val ) >= 3:
-                ans[key] = val
-
-        mx = max(len(val) for val in freq.values())
-        if mx < 3:
-            return - 1
-        mx = float("inf")
-        for key , val in ans.items():
-            mn = float("inf")
-            for i in range(len(val) - 2):
-                x = abs(val[i] - val[i+1]) + abs(val[i] - val[i+2]) + abs(val[i+1] - val[i+2])
-                mn = min(mn , x)
-            mx = min(mx, mn)
-        return mx
+            if len(val) >= 3:
+                for i in range(len(val) - 2):
+                    x, y, z = val[i], val[i + 1], val[i + 2]
+                    if abs(x-y) + abs(y- z) + abs(z - x) < mn:
+                        mn =  abs(x-y) + abs(y- z) + abs(z - x) 
+        print(mn)
+        return mn

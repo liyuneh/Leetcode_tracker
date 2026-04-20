8fe09c9d707abcc25478a275657bc8c623edeb19
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-04-20 12:10:08 +0300

    Time: 139 ms (61.56%) | Memory: 28.2 MB (62.60%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
@@ -3,13 +3,10 @@ class Solution:
         def rev (x):
             x = str(x)[::-1]
             return int(x)
-        for i ,ch in enumerate(nums):
-            nums[i] = nums[i] - rev(nums[i])
-        freq = defaultdict(int)
-        for ch in nums:
-            freq[ch] += 1
         count = 0
-        for ch in nums:
-            freq[ch] -= 1
-            count += freq[ch]
+        freq = defaultdict(int)
+        for i ,ch in enumerate(nums):
+            val = nums[i] - rev(nums[i])
+            count += freq[val]
+            freq[val] += 1
         return count % (10 ** 9 + 7)

f248e21bd58b0f93b29fcfd38ce69444e6e8c4a6
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-04-28 17:06:01 +0300

    Time: 4 ms (48.14%) | Memory: 20 MB (99.77%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        incoming = [0 for _ in range(numCourses)]
+        q = deque()
+        sortedone = []
+        for course , pre in prerequisites:
+            graph[pre].append(course)
+            incoming[course] += 1
+        
+        for course in range(numCourses):
+            if incoming[course] == 0:
+                q.append(course)
+        while q:
+            course = q.popleft()
+            sortedone.append(course)
+
+            for ne in graph[course]:
+                incoming[ne] -= 1
+                if incoming[ne] == 0:
+                    q.append(ne)
+        if len(sortedone) != numCourses:
+            return []
+
+
+        return sortedone

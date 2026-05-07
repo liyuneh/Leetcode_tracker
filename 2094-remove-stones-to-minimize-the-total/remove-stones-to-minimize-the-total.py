1c71a6da11f4743e7014b1199d1f41211f08130d
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-05-07 23:30:38 +0300

    Time: 586 ms (48.38%) | Memory: 34.3 MB (10.90%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -1,24 +1,12 @@
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        def heapify_down(nums, n, i):
-            cur , l, r = i, 2 * i + 1, 2 * i + 2
-            if l < n and nums[l] > nums[cur]:
-                cur = l
-            if r < n and nums[r] > nums[cur]:
-                cur = r
-            if cur != i:
-                swap(nums, i, cur)
-                heapify_down(nums,len(nums), cur)
-        def swap(nums, i , j):
-            nums[i], nums[j] = nums[j], nums[i]
-        
-        def replace(nums, val):
-            nums[0] = val
-            heapify_down(nums, len(nums), 0)
-        for i in range(len(piles) // 2, -1, -1):
-            heapify_down(piles, len(piles), i)
-        while k > 0:
-            replace(piles, piles[0] - piles[0] // 2)
+        piles = [-x for x in piles]
+        heapq.heapify(piles)
+
+        while k:
+            x = -heapq.heappop(piles)
+            x -= x // 2
+
+            heapq.heappush(piles, -x)
             k -= 1
-        # print(sum(piles), piles)
-        return sum(piles)
+        return -sum(piles)

198124f51555d19684f402fbf68b592e7f5393d4
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-05-17 14:12:26 +0300

    Time: 11 ms (65.93%) | Memory: 26.6 MB (47.47%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -2,17 +2,18 @@ class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         n = len(arr)
         def inbound(i): return 0 <= i < n
-        value = set()
-        for i in range(n):
-            if arr[i] == 0:
-                value.add(i)
-        visited = set()
-        def dfs(i):
-            if not inbound(i) or i in visited:
-                return False
+        q = deque([start])
+        visited = set([start])
+
+        while q:
+            i = q.popleft()
             if arr[i] == 0:
                 return True
-            visited.add(i)
-            return dfs(i + arr[i]) or dfs(i - arr[i])
-        
-        return dfs(start)
+            one , two = i - arr[i], i + arr[i]
+            if inbound(one) and one not in visited:
+                visited.add(one)
+                q.append(one)
+            if inbound(two) and two not in visited:
+                visited.add(two)
+                q.append(two)
+        return False

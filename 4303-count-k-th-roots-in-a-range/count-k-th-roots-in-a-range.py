354541848e4fce59a5ce3721f16f152a9b81f8dc
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-05-20 11:44:00 +0300

    Time: 7 ms (30.24%) | Memory: 19.2 MB (97.58%) - LeetSync

--- 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.py
+++ 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.py
@@ -4,11 +4,8 @@ class Solution:
             return r - l + 1
         else:
             a = math.ceil((r) ** ( 1 / k))
-            print(a)
             count = 0
             for i in range(int(a) + 1):
-                print(i)
                 if l <= (i) ** k <= r:
                     count += 1
-            print(count)
             return count

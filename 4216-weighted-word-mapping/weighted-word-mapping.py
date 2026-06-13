a251cd1c7b4664a8834630e4af71a078cc854b78
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-06-13 10:41:02 +0300

    Time: 9 ms (56.74%) | Memory: 19.4 MB (33.64%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -5,9 +5,5 @@ class Solution:
             count = 0
             for j in range(len(words[i])):
                 count += weights[ord(words[i][j]) - ord("a")]
-            ans.append(count % 26)
-        new = []
-        for i in range(len(ans)):
-            x = chr(97 + 25 - ans[i])
-            new.append(x)
-        return "".join(new)
+            ans.append(chr(97 + 25 - count % 26))
+        return "".join(ans)
